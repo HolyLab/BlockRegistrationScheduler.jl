a48f7ba284fdc6269e47376567ab6422ac0e2c50
Tim Holy <tim.holy@gmail.com>
2015-10-15 07:24:05 -0500

    Name deps dir properly
    Gets Pkg.build("BlockRegistrationScheduler") working

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,4 @@
 if Base.find_in_path("BlockRegistration") == nothing
     Pkg.clone("git@github.com:HolyLab/BlockRegistration.git")
-    include(Pkg.dir("BlockRegistration","build","build.jl"))
+    Pkg.build("BlockRegistration")
 end
-Pkg.checkout("JLD")  # need master branch
