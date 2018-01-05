74d1ca3a37756e9661683c596858adb0b73ae7b4
Cody-G <codyjgreer@gmail.com>
2018-01-05 11:58:39 -0600

    Add a new rigid registration algorithm RigidGridStart that initializes with a gridsearch of rotations+shifts.

--- test/rigid.jl
+++ test/rigid.jl
@@ -3,11 +3,32 @@ using BlockRegistration
 using BlockRegistrationScheduler, RegisterDriver, RegisterWorkerShell, RegisterWorkerRigid
 using Base.Test
 
+#Rigid
 fixed = testimage("cameraman")
 tfm = tformrotate(pi/12)
 moving = transform(fixed, tfm)
 
 alg = Rigid(fixed, pat=false, print_level=5)
+mon = monitor(alg, ())
+mon[:tform] = nothing
+mon[:mismatch] = 0.0
+mon = driver(alg, moving, mon)
+ptfm = mon[:tform]*tfm
+@test ≈(ptfm.scalefwd, eye(2), atol=0.01)
+@test all(abs.(ptfm.offset) .< 0.2)
+@test mon[:mismatch] < 0.001
+
+#RigidGridStart
+fixed = testimage("cameraman")
+tfm = tformrotate(pi/10)
+moving = transform(fixed, tfm)
+moving[find(x->isnan(x), moving)] = zero(eltype(moving))
+
+maxradians = pi/10
+rgridsz = 3
+mxshift = (3,3)
+alg = RigidGridStart(fixed, maxradians, rgridsz, mxshift; print_level=5)
+#alg = RigidGridStart(fixed, maxradians, 1, mxshift; print_level=5)
 mon = monitor(alg, ())
 mon[:tform] = nothing
 mon[:mismatch] = 0.0
